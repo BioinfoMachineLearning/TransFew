13409a0d098e209c92a8660d8a11b8b8ef51ac1e
fbqc9 <23377139+frimpz@users.noreply.github.com>
2023-04-20 21:09:53 -0500

    Removed CONSTANTS

--- workbook.py
+++ workbook.py
@@ -3,10 +3,11 @@ import os
 import shutil
 
 import pandas as pd
+from Bio import SeqIO
 
 import CONSTANTS
 from Classes.Fasta import Fasta
-from Utils import count_proteins
+from Utils import count_proteins, create_seqrecord
 from preprocessing.utils import pickle_save
 
 
@@ -22,4 +23,22 @@ def sup_1():
     embeddings.subset_from_fasta(CONSTANTS.ROOT_DIR + "uniprot/uniprot_gt_1022.fasta")
 
 
-sup_1()
+# sup_1()
+
+
+def divide_4_interpro():
+    seqs = []
+    input_seq_iterator = SeqIO.parse("/home/fbqc9/PycharmProjects/ipscan/uniprot_fasta.fasta", "fasta")
+    for pos, record in enumerate(input_seq_iterator):
+        seqs.append(create_seqrecord(id=record.id, seq=str(record.seq)))
+        if pos % 10000 == 0 and pos != 0:
+            SeqIO.write(seqs, CONSTANTS.ROOT_DIR + "uniprot/{}_{}.fasta".format("uniprot_fasta", pos), "fasta")
+            seqs = []
+
+    SeqIO.write(seqs, CONSTANTS.ROOT_DIR + "uniprot/{}_{}.fasta".format("uniprot_fasta", pos), "fasta")
+
+# divide_4_interpro()
+
+# print(count_proteins("/home/fbqc9/PycharmProjects/ipscan/uniprot_fasta.fasta"))
+
+print(count_proteins(CONSTANTS.ROOT_DIR + "uniprot/{}_{}.fasta".format("uniprot_fasta", 79220)))
